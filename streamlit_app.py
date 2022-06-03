e589dab1d32b632a66396d1003182b300e6eb4da
Filipe Carvalho <67872593+filipebodas@users.noreply.github.com>
2022-06-03 14:44:20 +0100

    Update streamlit_app.py
    correct pandas to pd typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 #streamlit.text(fruityvice_response.json()) #just writes the data on the screen - an one row json format
 
 #Take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 #Output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
